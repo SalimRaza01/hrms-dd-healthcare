1aecb34691cd30c548fd6ada7a13f857b896b9e3
Salim-agva <salim.creation2017@gmail.com>
2024-12-19 09:47:23 +0530

    added regularization ui, punch record ui improved

--- lib/core/api/api_config.dart
+++
@@ -1,7 +0,0 @@
-final url = 'http://172.23.100.121:3000';
-
-final getAttendenceData = '$url/api/attendance-logs';
-final getEmployeeData = '$url/api/employee/get-employee-details';
-final employeeLogin = '$url/api/employee/login';
-final getLeaveHistory = '$url/api/leave/get-employee-leave';
-final employeeApplyLeave = '$url/api/leave/apply-leave';
